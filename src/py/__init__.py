6b3f903c3c1d891227bd0f866fb460daf789d314
TRIAEIOU <triaeiou@protonmail.com>
2022-12-12 23:48:39 +0100

    Rework version check

--- src/py/__init__.py
+++ src/py/__init__.py
@@ -7,7 +7,7 @@ from .constants import *
 mw.addonManager.setWebExports(__name__, r"(.*(css|js|map))")
 config = mw.addonManager.getConfig(__name__)
 
-if config['Field input'].get("Restore state on toggle", None) != None:
+if strvercmp(config.get('version', '1.1.0'), '1.2.0') < 0:
     msg_box = QMessageBox(mw)
     msg_box.setWindowTitle('Addon "Markdown input" updated')
     msg_box.setText("""<div style="text-align: left;">"Markdown input" addon has been updated, potentially changing the behaviour depending on your current configuration for the addon.<ul><li>The configurations "Restore state on toggle", "Hide plain text on toggle" and "Hide rich text on toggle" for "Field input" have been replaced with "Cycle rich text/Markdown" (making the Markdown shortcut either cycle between rich text and Markdown or simply show/hide the Markdown input).</li><li>"Cloze lists" for "Converter" has been removed.</li></ul>Please see the addon page (https://ankiweb.net/shared/info/904999275) for details.</div>""")
@@ -18,7 +18,7 @@ if config['Field input'].get("Restore state on toggle", None) != None:
     config['Field input'].pop('Hide rich text on toggle', None)
     config['Converter'].pop('Cloze lists', None)
 
-if strvercmp(config.get('version', '1.1.0'), VERSION) < 0:
+if strvercmp(config.get('version', '0.0.0'), VERSION) < 0:
     config['version'] = VERSION
     mw.addonManager.writeConfig(__name__, config)
 
