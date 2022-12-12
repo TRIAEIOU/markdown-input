8b8bc6d36d44892af8caad3c20ab5e609e1e8c40
TRIAEIOU <triaeiou@protonmail.com>
2022-12-12 23:40:34 +0100

    Add version check.

--- src/py/__init__.py
+++ src/py/__init__.py
@@ -1,20 +1,33 @@
-from aqt import mw
+from aqt import mw, QPushButton, QMessageBox
+
 from aqt.utils import *
 from . import field_input, dialog_input
 from .constants import *
-#from .utils import tracefunc
-#import sys
-#sys.setprofile(tracefunc)
 
 mw.addonManager.setWebExports(__name__, r"(.*(css|js|map))")
 config = mw.addonManager.getConfig(__name__)
+
+if config['Field input'].get("Restore state on toggle", None) != None:
+    msg_box = QMessageBox(mw)
+    msg_box.setWindowTitle('Addon "Markdown input" updated')
+    msg_box.setText("""<div style="text-align: left;">"Markdown input" addon has been updated, potentially changing the behaviour depending on your current configuration for the addon.<ul><li>The configurations "Restore state on toggle", "Hide plain text on toggle" and "Hide rich text on toggle" for "Field input" have been replaced with "Cycle rich text/Markdown" (making the Markdown shortcut either cycle between rich text and Markdown or simply show/hide the Markdown input).</li><li>"Cloze lists" for "Converter" has been removed.</li></ul>Please see the addon page (https://ankiweb.net/shared/info/904999275) for details.</div>""")
+    msg_box.addButton(QPushButton('Ok'), QMessageBox.YesRole)
+    msg_box.exec()
+    config['Field input'].pop('Restore state on toggle', None)
+    config['Field input'].pop('Hide plain text on toggle', None)
+    config['Field input'].pop('Hide rich text on toggle', None)
+    config['Converter'].pop('Cloze lists', None)
+
+if strvercmp(config.get('version', '1.1.0'), VERSION) < 0:
+    config['version'] = VERSION
+    mw.addonManager.writeConfig(__name__, config)
 
 if config.get(CONVERTER) and config[CONVERTER][HARDBREAK] != 'spaces':
     config[CONVERTER][HARDBREAK] = 'backslash'
 
 if config.get(FIELD_INPUT) or config.get(CONVERTER):
     field_input.init(config)
- 
+
 if config.get(DIALOG_INPUT) or config.get(CONVERTER):
     dialog_input.init(config)
 
--- src/py/constants.py
+++ src/py/constants.py
@@ -26,5 +26,22 @@ NEXT_SHORTCUT = "Next field"
 PREV_SHORTCUT = "Previous field"
 CONVERTER = "Converter"
 EDITOR = "CodeMirror"
-
 ADDON_PATH = os.path.dirname(__file__)
+VERSION = "1.2.0"
+
+def strvercmp(left: str, right: str) -> int:
+    """Compares semantic version strings.\n
+    Returns:    left string is larger: >0
+                right string is larger: <0
+                strings are equal: 0"""
+    import re
+    l = re.match("^([0-9]+)\.([0-9]+)\.([0-9]+)([ab])?([0-9]+)?$", left).groups()
+    r = re.match("^([0-9]+)\.([0-9]+)\.([0-9]+)([ab])?([0-9]+)?$", right).groups()
+    for i in range(3):
+        if l[i] != r[i]:
+            return 1 if int(l[i]) > int(r[i]) else -1
+    if l[3] != r[3]:
+        return 1 if l[3] == None or (r[4] != None and l > r) else -1
+    if l[4] != r[4]:
+        return 1 if r[4] == None or (l[4] != None and int(l[4]) > int(r[4])) else -1
+    return 0
